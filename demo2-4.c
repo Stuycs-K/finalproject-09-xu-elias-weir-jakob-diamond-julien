81dd5a078fe379b679c73c42de52c9655c832383
Julien Diamond <jdiamond50@stuy.edu>
2025-06-01 18:16:41 -0400

    finalized demo2 (version demo2-4)

---
+++ demo2-4.c
@@ -0,0 +1,20 @@
+// gcc demo2-4.c -o demo2-4 -zexecstck -fno-stack-protector -no-pie -g
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int specialFunction() {
+  puts("i'm special");
+  exit(0);
+}
+
+int getInput() {
+  char buffer[8];
+  gets(buffer);
+  puts(buffer);
+}
+
+int main() {
+  getInput();
+  return 0;
+}
